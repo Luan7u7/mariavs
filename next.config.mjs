61a38c7fd99c8603c70fc93220f54cf4d263105c
Luan Henrique <luanhenriquevg@gmail.com>
2025-06-13 01:27:05 +0100

    add dist directory in ignore and add distDir and basePath in nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production"
+
 const nextConfig = {
+  basePath: isProduction ? "/mariavs" : "",
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
@@ -10,6 +14,10 @@ const nextConfig = {
   //   unoptimized: true,
   // },
   output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
